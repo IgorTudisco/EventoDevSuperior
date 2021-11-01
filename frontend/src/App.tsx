e3721a0a8636882a5c3b82cdab85ae20c2bd8333
Igor Tudisco <igortudisco@hotmail.com>
2021-11-01 16:15:00 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary"> Olá Zika! </h1>
-    </div>
+    // Como no React não aceita mais de um elemento no retorno, temos que usar essa tag. (fragment)
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary"> Olá Zika! </h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
